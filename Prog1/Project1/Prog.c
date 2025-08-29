6e910b6cb54cedaca6183ecc83621f0ac841d95b
ohfujimasa73 <ohfuji.masa73@gmail.com>
2025-08-30 08:09:18 +0900

    修正コミット

--- Prog1/Project1/Prog.c
+++ Prog1/Project1/Prog.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
+#pragma warning(disable:4996)
 
 int main() {
-	int a = 5;
-	int b = 10;
-	int sum = a + b;
-	printf("Sum: %d\n", sum);
-	return 0;
+    int length;
+    char  moji[20] = {'\0'}, * pmoji;
+
+
+    printf("10文字程度の英数字の文字列を入力してください（スペースなしで）\n");
+    (void)scanf("%s", moji);
+
+	/* 文字列の長さを取得 */
+    if ( moji != NULL && ((length = (int)strlen(moji)) == 0) ) {
+        return 0;
+    }
+    /* ポインタに文字列のアドレスを代入 */
+    pmoji = moji;
+
+    printf("入力された文字列を逆順に表示します。\n");
+    for (int loop = 1; loop <= length; loop++) {
+        printf("%s\n", (pmoji + (char)length - (char)loop));
+    }
+
+    return 0;
 }
